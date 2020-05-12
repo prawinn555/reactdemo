72b8c6909a3dc3cbc57510c51c264bf9bb48acb6
lapin <lapin@psriplakich-Pc.npif.local>
2020-05-12 11:37:05 +0200

    production env

--- src/env.js
+++ src/env.js
@@ -4,5 +4,5 @@ export var serviceUrl =   // 'http://localhost:1234/';
      'https://generic-db.glitch.me/';
 
 
-export var moviesServiceUrl =   'http://localhost:1234/api/movies'; 
-     // 'https://verceltest-smoky.now.sh/api/movies';
+export var moviesServiceUrl =   // 'http://localhost:1234/api/movies'; 
+     'https://verceltest-smoky.now.sh/api/movies';
